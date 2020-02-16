572cb5ee1ad9d32f198dda1a376d136d9bd8d6a0
Bruno Serrano <bruno.santos@supera.com.br>
2020-02-16 12:32:09 -0300

    Init

---
+++ tests-ios-products/app-modules/products/view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tests-ios-products
+//
+//  Created by Bruno Serrano dos Santos on 16/02/20.
+//  Copyright © 2020 EagleSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
